ea828af808731cac1914ab24be8bd3ca08b0dd29
Paulina Loor Tubay <108084639+LoorPaulina@users.noreply.github.com>
2024-06-27 22:45:52 -0500

    costo and discount

--- main.py
+++ main.py
@@ -45,6 +45,45 @@ class GymMembership:
         for feature, cost in self.additional_features.items():
             print(f"{feature}: ${cost}")
 
+    def get_discount(self):
+        """Function get discount"""
+        if self.total_cost > 400:
+            self.total_discount = 50
+        elif self.total_cost > 200:
+            self.total_discount = 20
+        else:
+            self.total_discount = 0
+        self.total_cost -= self.total_discount
+
+
+    def premiun_membership(self):
+        """Function apply an additional 15% in plan premium"""
+        self.total_cost = self.total_cost * 1.15
+
+    def calculate_cost(self):
+        """Function calculate cost"""
+        if not self.selected_plan:
+            raise ValueError("No membership plan selected.")
+
+        base_cost = self.membership_plans[self.selected_plan]['cost']
+        additional_features_cost = sum(
+            self.additional_features[feature] for feature in self.selected_features
+        )
+
+
+        cost_members = base_cost * self.num_members
+        self.total_cost = cost_members + (additional_features_cost * self.num_members)
+
+        if self.num_members >= 2:
+            self.total_cost *= 0.9  # Apply 10% discount for group memberships
+
+        self.get_discount()
+
+        if self.selected_plan == 'Premium':
+            self.premiun_membership()
+
+        return self.total_cost
+
 
     def add_additional_features(self, features):
         """Function add additional features"""
